cd369ac246790f0df00d3bf617c1016f55902b92
Fantasos <michalkaczor7@gmail.com>
2014-05-01 23:48:14 +0200

    Added map loading

--- core/src/com/arkona/ArkonaGame.java
+++ core/src/com/arkona/ArkonaGame.java
@@ -3,7 +3,10 @@ package com.arkona;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
 public class ArkonaGame extends Game {
 
@@ -15,8 +18,10 @@ public class ArkonaGame extends Game {
 	public void create () {
 
 		assets = new AssetManager();
+		assets.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
 
 		assets.load("arkona_logo.png", Texture.class);
+		assets.load("testmap1.tmx", TiledMap.class);
 
 		while (!assets.update()) {
 			Gdx.app.log("Assets", assets.getProgress() * 100 + "%");
